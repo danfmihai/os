c705bf5f92f374dd7fa4477b61a8a63400150d62
yura <yura407@gmail.com>
2020-12-10 18:23:04 -0500

    fixed board name

--- mk-gen-aarch64.sh
+++ mk-gen-aarch64.sh
@@ -2,4 +2,4 @@ make os BOARD=generic ARCH=aarch64
 make image BOARD=generic ARCH=aarch64 UBOOT=nanopi-r2s-rk3328
 make image BOARD=generic ARCH=aarch64 UBOOT=rock64
 make image BOARD=generic ARCH=aarch64 UBOOT=roc-cc-rk3328
-make image BOARD=generic ARCH=aarch64 UBOOT=rockpi-e
+make image BOARD=generic ARCH=aarch64 UBOOT=rock-pi-e-rk3328
